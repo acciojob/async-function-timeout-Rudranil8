0fa6bbd462aeb85557902b9f2a3c4acb92928169
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-28 16:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', displayText);
+
+async function displayText() {
+	let text = document.getElementById('text').value;
+	let delay = document.getElementById('delay').value;
+}
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(), delay);
+});
+await promise;
+document.getElementById('output').innerHTML = text;
